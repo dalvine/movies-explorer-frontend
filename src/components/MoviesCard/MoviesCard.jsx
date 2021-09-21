c333864247095e0b354934dec52547ac283ebb85
dalvine <dalvine@ya.ru>
2021-09-21 14:52:18 +0300

    feat: added list with movies

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import './MoviesCard.css';
 
-function MoviesCard() {
-  return (
-    <div className="MoviesCard">
+function MoviesCard({title, duration, image}) {
+  const [like, setLike] = React.useState(false)
 
-    </div>
+  function changeLike() {
+    setLike(!like)
+  }
+
+  return (
+    <li className="movies__card">
+      <h2 className="movies__title">{ title }</h2>
+      <p className="movies__duration">{ duration }</p>
+      <div className={`movies__like ${like ? 'movies__like_active': ''}`} onClick={changeLike}></div>
+      <img className="movies__image" src={image} alt="logo" />
+    </li>
   );
 }
 
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,11 +1,18 @@
 import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard'
+import logoMovie from '../../images/logo-movie.png'
 
 function MoviesCardList() {
   return (
-    <div className="MoviesCardList">
-
-    </div>
+    <>
+      <ul className="movies__list">
+        <MoviesCard title="33 слова о дизайне" duration="1ч 42м" image={logoMovie}  />
+        <MoviesCard title="Киноальманах «100 лет дизайна»" duration="1ч 42м" image={logoMovie}  />
+        <MoviesCard title="В погоне за Бенкси" duration="1ч 42м" image={logoMovie} />
+      </ul>
+      <button className="movies__more">Ещё</button>
+    </>
   );
-}
+} 
 
 export default MoviesCardList;
