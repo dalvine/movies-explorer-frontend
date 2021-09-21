7f00a6b59831f0da2f535ada1d578ab70128fca5
dalvine <dalvine@ya.ru>
2021-09-21 14:52:55 +0300

    feat: added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,17 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="Footer">
-
-    </div>
+    <footer className="footer">
+      <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__container">
+        <p className="footer__copyright">&copy; 2021</p>
+        <ul className="footer__list">
+          <li className="footer__item"><a href="#" className="footer__link">Яндекс.Практикум</a></li>
+          <li className="footer__item"><a href="#" className="footer__link">Github</a></li>
+          <li className="footer__item"><a href="#" className="footer__link">Facebook</a></li>
+        </ul>
+      </div>
+    </footer>
   );
 }
 
