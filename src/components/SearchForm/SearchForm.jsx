bce40d7a863b9fe8a5334797f424563b1c259cf1
dalvine <dalvine@ya.ru>
2021-09-21 14:50:53 +0300

    feat: added search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,22 @@
 import './SearchForm.css';
+import icoSearch from '../../images/search-ico.svg'
 
 function SearchForm() {
   return (
-    <div className="SearchForm">
-
+    <div className="search">
+      <form className="search__form">
+        <img className="search__ico" src={icoSearch} alt="поиск" />
+        <input className="search__input" type="text" name="value" placeholder="Фильм"/>
+        <button className="search__submit" type="submit">Найти</button>
+        <div className="search__divider"></div>
+        <input className="search__checkbox" type="checkbox" name="shortFilm" id="shortFilm" />
+        <label className="checkbox" htmlFor="shortFilm">
+          <div className="checkbox__switch">
+            <div className="checkbox__tumbler"></div>
+          </div>
+          <p className="checkbox__text"> Короткометражки</p>
+        </label>
+      </form>
     </div>
   );
 }
