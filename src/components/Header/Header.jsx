955a41806637e78ccdd6c7bc0a0a531ce0830bb8
dalvine <dalvine@ya.ru>
2021-09-22 14:56:49 +0300

    feat: added login panel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,22 @@ import './Header.css';
 import Logo from '../Logo/Logo' 
 import UserAccount from '../UserAccount/UserAccount'
 import Navigation from '../Navigation/Navigation'
+import LoginPanel from '../LoginPanel/LoginPanel'
 
 
-function Header() {
+function Header({ loggedIn }) {
   return (
-    <header className="header">
+    <header className={`header ${loggedIn || 'header_theme_dark'}`}>
       <Logo />
+      {loggedIn ? 
+      <>
       <Navigation />
       <UserAccount />
+      </>
+      :
+      <LoginPanel />
+      }
+
     </header>
   );
 }
---
+++ src/components/LoginPanel/LoginPanel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginPanel() {
+  return (
+    <div className="login-panel">
+      <button className="login-panel__reg">Регистрация</button>
+      <button className="login-panel__login">Войти</button>
+    </div>
+  )
+}
+
+export default LoginPanel
