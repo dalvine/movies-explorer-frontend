7d7e0e8775cb8bc153e3a39d88cb197ef128e029
dalvine <dalvine@ya.ru>
2021-09-22 14:57:43 +0300

    feat: add block about-project

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -2,8 +2,22 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <div className="AboutProject">
-
+    <div className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <div className="about-project__descriptions">
+        <div className="about-project__description">
+          <p className="about-project__intro">Дипломный проект включал 5 этапов</p>
+          <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </div>
+        <div className="about-project__description">
+          <p className="about-project__intro">На выполнение диплома ушло 5 недель</p>
+          <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
+      <div className="story-line">
+        <p className="story-line__backend">1 неделя</p>
+        <p className="story-line__frontend">4 неделя</p>
+      </div>
     </div>
   );
 }
