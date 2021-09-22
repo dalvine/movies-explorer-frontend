7761e924b52da00392cb23dd8c51127e4653554b
dalvine <dalvine@ya.ru>
2021-09-22 14:59:19 +0300

    feat: add block tehcs

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -2,8 +2,19 @@ import './Techs.css';
 
 function Techs() {
   return (
-    <div className="Techs">
-
+    <div className="techs">
+      <h2 className="techs__title">Технологии</h2>
+      <p className="techs__intro">7 технологий</p>
+      <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="techs_tags">
+        <li className="techs_tag">HTML</li>
+        <li className="techs_tag">CSS</li>
+        <li className="techs_tag">JS</li>
+        <li className="techs_tag">React</li>
+        <li className="techs_tag">Git</li>
+        <li className="techs_tag">Expres.js</li>
+        <li className="techs_tag">mongoDB</li>
+      </ul>
     </div>
   );
 }
