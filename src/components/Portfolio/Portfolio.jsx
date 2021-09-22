5611d08bf9f256d14326098ed7ba59988bd33790
dalvine <dalvine@ya.ru>
2021-09-22 14:58:08 +0300

    feat: add block portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
+import React from 'react';
 import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <div className="Portfolio">
-
+    <div className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__link-list">
+        <li className="portfolio__link-list-item">
+          <a className="portfolio__link" href="#">Статичный сайт</a>
+        </li>
+        <li className="portfolio__link-list-item">
+          <a className="portfolio__link" href="#">Адаптивный сайт</a>
+        </li>
+        <li className="portfolio__link-list-item">
+          <a className="portfolio__link" href="#">Одностраничное приложение</a>
+        </li>
+      </ul>
     </div>
   );
 }
