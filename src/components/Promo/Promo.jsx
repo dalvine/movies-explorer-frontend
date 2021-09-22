3d5cf0e46aa2f581ff5d5eb1f51d49589866f26d
dalvine <dalvine@ya.ru>
2021-09-22 14:53:08 +0300

    feat: added promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,9 +1,13 @@
 import './Promo.css';
+import promoImage from "../../images/promo-WEB.png";
 
 function Promo() {
   return (
-    <div className="Promo">
-
+    <div className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      <button className="promo__more">Узнать больше</button>
+      <img src={promoImage} alt="Картинка промо" className="promo__image" />
     </div>
   );
 }
