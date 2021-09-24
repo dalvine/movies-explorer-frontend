01369dee6a74f3d9578e78c17b7c366f28f9d4d6
dalvine <dalvine@ya.ru>
2021-09-24 12:53:44 +0300

    feat: added side menu for mobile resolution

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,15 @@
 import './Navigation.css';
 
-function Navigation() {
+function Navigation({handleOpenedMenu, isOpenedMenu}) {
   return (
-    <ul className="navigation">
+    <>
+    <button className={`btn-opened-menu ${isOpenedMenu ? 'btn-opened-menu_active' : ''}`} onClick={handleOpenedMenu}></button>
+    <ul className={`navigation ${isOpenedMenu ? 'navigation_opened' : ''}`}>
+      <li className="navigation__item navigation__item_content_main">Главная</li>
       <li className="navigation__item navigation__item_active">Фильмы</li>
       <li className="navigation__item">Сохраненные фильмы</li>
     </ul>
+    </>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,23 @@
 import './Profile.css';
 
-function Profile() {
+function Profile({name, email}) {
   return (
-    <div className="Profile">
-
+    <div className="profile">
+      <h1 className="profile__greeting">Привет, {name ? 'name' : 'Владислав'}!</h1>
+      <div className="profile__information">
+        <div className="profile__item">
+          <p className="profile__category">Имя</p>
+          <p className="profile__value">{name ? 'name' : 'Владислав'}</p>
+        </div>
+        <div className="profile__item">
+          <p p className="profile__category">E-mail</p>
+          <p className="profile__value">{email ? 'name' : 'pochta@yandex.ru'}</p>
+        </div>
+      </div>
+      <div className="profile__control">
+        <button className="profile__edit">Редактировать</button>
+        <button className="profile__exit">Выйти из аккаунта</button>
+      </div>
     </div>
   );
 }
